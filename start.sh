2eda612d85001544bffc1c1b8e4ef91165ce79a0
Telcollection <96669900+Telcollection@users.noreply.github.com>
2023-03-07 00:16:10 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Cyniteofficial/Auto-Filter-V5-Without-UrlShortner.git /Auto-Filter-V5-Without-UrlShortner
+  git clone https://github.com/Telcollection/Auto-Filter-V5.git /Auto-Filter-V5
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Auto-Filter-V5-Without-UrlShortner
+  git clone $UPSTREAM_REPO /Auto-Filter-V5
 fi
 cd /Auto-Filter-V5
 pip3 install -U -r requirements.txt
